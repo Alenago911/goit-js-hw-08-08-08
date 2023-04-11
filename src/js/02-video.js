c799ad5539330bb7ffe8da4b75930da85c1a0c81
Alenago911 <alena.chernyavka@gmail.com>
2023-04-11 18:24:51 +0300

    2 video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const event = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+const roundetTime = Math.round(data.seconds);
+localStorage.setItem(event, roundetTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const time = localStorage.getItem(event);
+player.setCurrentTime(time);
